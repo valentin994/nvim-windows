eeb35f400b405c7a50e3454c454a2d0482d67638
Valentin Vareskic <valentin.vareskic@gmail.com>
2024-02-12 10:23:36 +0100

    fix

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,7 +5,4 @@ require("plugins.lsp")
 require("plugins.terminal")
 require("plugins.telescope")
 require("plugins.theme")
-<<<<<<< HEAD
-=======
 require("plugins.gitblame")
->>>>>>> 1d71a68153c05cae78e2032897de218d0a224b5c
