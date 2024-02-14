839c9bcaa27db8640723777d11319bd578e702ab
Valentin Vareskic <valentin.vareskic@gmail.com>
2024-02-14 14:35:14 +0100

    updated nerdtree keymap

--- lua/plugins/keymap.lua
+++ lua/plugins/keymap.lua
@@ -1,2 +1,4 @@
-vim.api.nvim_set_keymap("n", "<leader>e", ":e .<CR>", { noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>e", ":NERDTreeToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-n>", ":NERDTree<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-f>", ":NERDTreeFind<CR>", { noremap = true, silent = true })
 
